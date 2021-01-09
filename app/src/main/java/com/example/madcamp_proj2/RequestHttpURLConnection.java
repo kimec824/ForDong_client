122489697d2fbfbaaf75bfd8f63efb592552af3b
gusals005 <gusals005@gmail.com>
2021-01-09 19:47:45 +0900

    210109_three_tab

--- app/src/main/java/com/example/madcamp_proj2/RequestHttpURLConnection.java
+++ app/src/main/java/com/example/madcamp_proj2/RequestHttpURLConnection.java
@@ -38,6 +38,7 @@ public class RequestHttpURLConnection {
         }
         return null;
     }
+
     public String request_post(String _url, ContentValues _params){
         String result = null;
         try {
@@ -100,5 +101,30 @@ public class RequestHttpURLConnection {
         }
 
         return result;
+    }
+
+    public String request_photoarray_get(String _url, ContentValues _params){
+        try {
+            URL url = new URL(_url);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET"); //전송방식
+            //connection.setDoOutput(true);       //데이터를 쓸 지 설정
+            connection.setDoInput(true);        //데이터를 읽어올지 설정
+
+            InputStream is = connection.getInputStream();
+            StringBuffer sb = new StringBuffer();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+            String result;
+            while((result = br.readLine())!=null){
+                sb.append(result);
+            }
+            result = sb.toString();
+            return result;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
